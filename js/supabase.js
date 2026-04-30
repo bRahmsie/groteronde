a882a19382e9ef8b93357e7053bb3423f55690d4
bRahmsie <segers.bram@gmail.com>
2026-04-30 16:00:46 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -6,6 +6,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL  = 'https://qxpfcqiujkeybrmlvsee.supabase.co';   // bv. https://xyzxyz.supabase.co
-const SUPABASE_ANON = 'sb_secret_Gapa-XvjU-yhLVH-EvxaPg_FwiA7Vlm';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4cGZjcWl1amtleWJybWx2c2VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MzcyMDQsImV4cCI6MjA5MzExMzIwNH0.oYlrR5Honyq_5L_jzwPRi6Ww12qebNKhRHXTenCulf4';
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
